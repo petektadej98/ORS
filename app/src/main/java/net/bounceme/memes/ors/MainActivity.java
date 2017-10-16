abe158f2821824e9387e56e0c797b1313c3762fc
Tadej Petek <petektadej98@gmail.com>
2017-10-16 09:15:35 +0200

    First commit

--- app/src/main/java/net/bounceme/memes/ors/MainActivity.java
+++ app/src/main/java/net/bounceme/memes/ors/MainActivity.java
@@ -1,5 +1,6 @@
 package net.bounceme.memes.ors;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
