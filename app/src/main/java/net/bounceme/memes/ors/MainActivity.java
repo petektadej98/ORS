194cb6c5ac9a39f4863d7fa2bbe9eb8e363a3d5b
Tadej Petek <petektadej98@gmail.com>
2017-10-22 13:53:27 +0200

    First commit

--- app/src/main/java/net/bounceme/memes/ors/MainActivity.java
+++ app/src/main/java/net/bounceme/memes/ors/MainActivity.java
@@ -1,15 +1,27 @@
 package net.bounceme.memes.ors;
 
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+
     }
 
 
